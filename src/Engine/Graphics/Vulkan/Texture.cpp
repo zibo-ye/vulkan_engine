0285c2db29ba39c899ea3f7347a5ff98de20a324
Zibo Ye <32037915+zibo-ye@users.noreply.github.com>
2024-02-17 13:31:09 -0500

    Texture class start

---
+++ src/Engine/Graphics/Vulkan/Texture.cpp
@@ -0,0 +1,2 @@
+#include "Texture.hpp"
+#include "VulkanCore.hpp"
---
+++ src/Engine/Graphics/Vulkan/Texture.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "pch.hpp"
+
+class VulkanCore;
+
+class Texture {
+
+public:
+    VkImage image;
+    VkDeviceMemory imageMemory;
+    VkImageView imageView;
+    VkSampler sampler;
+    uint32_t mipLevels;
+    uint32_t texWidth, uint32_t texHeight;
+
+    std::shared_ptr<VulkanCore> vulkanCore;
+};
