a08b9e735443fbd5bd078b0da9c8d4e83022ee37
Zibo Ye <32037915+zibo-ye@users.noreply.github.com>
2024-02-25 14:36:16 -0500

    test script

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Get the full path of the directory where the script is located
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+source ~/.zshrc
+
+# Change the working directory to the parent of the script's directory
+cd "$SCRIPT_DIR"/..
+
+# Execute the command
+xmake run Main -scene ../s72/examples/sphereflake_animated.s72
